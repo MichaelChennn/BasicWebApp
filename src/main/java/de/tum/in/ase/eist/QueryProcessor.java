7ba45d4569aaeb6d63b687cdd740366ad822a630
yaxuan <chenyaxuan123@gmail.com>
2022-07-13 13:56:48 +0200

    fix

--- src/main/java/de/tum/in/ase/eist/QueryProcessor.java
+++ src/main/java/de/tum/in/ase/eist/QueryProcessor.java
@@ -1,7 +1,6 @@
 package de.tum.in.ase.eist;
 
 import java.util.Arrays;
-import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
@@ -20,6 +19,7 @@ public class QueryProcessor {
             return "CHEN";
         } else if (query.contains("plus")) {
             String s = query;
+            s = s.substring(10);
             s = s.replace("what is ", "");
             s = s.replace(" plus ", " ");
             s = s.trim();
@@ -29,6 +29,7 @@ public class QueryProcessor {
             return result;
         } else if (query.contains("largest")) {
             String s = query;
+            s = s.substring(10);
             s = s.replace("which of the following numbers is the largest: ", "");
             s = s.replace(", ", " ");
             s = s.trim();
@@ -47,22 +48,23 @@ public class QueryProcessor {
     }
 
     public static void main(String[] args) {
-            // String s = "which of the following numbers is the largest: 55, 464, 458, 69";
+            String s = "6861f020: which of the following numbers is the largest: 914, 61";
             // String s1 = "what is 8 plus 4";
-            // // s = s.replace("which of the following numbers is the largest: ", "");
-            // // s = s.replace(", ", " ");
-            // // s = s.trim();
-            // // String[] nummbers = s.split(" ");
-            // // int result = Integer.parseInt(nummbers[0]);
-            // // for (int i = 1; i < nummbers.length; i++) {
-            // //     if (Integer.parseInt(nummbers[i]) > result) {
-            // //         result = Integer.parseInt(nummbers[i]);
-            // //     }
-            // // }
-            // // String tmp = "" + result;
+            // s = s.replace("which of the following numbers is the largest: ", "");
+            // s = s.replace(", ", " ");
+            // s = s.trim();
+            // String[] nummbers = s.split(" ");
+            // int result = Integer.parseInt(nummbers[0]);
+            // for (int i = 1; i < nummbers.length; i++) {
+            //     if (Integer.parseInt(nummbers[i]) > result) {
+            //         result = Integer.parseInt(nummbers[i]);
+            //     }
+            // }
+            // String tmp = "" + result;
             // QueryProcessor queryProcessor = new QueryProcessor();
             // System.out.println(queryProcessor.process(s));
             // System.out.println(queryProcessor.process(s1));
+        
     }
 
 }
